b031d95f68e4715669036289971a3166da9ed6c9
Jonathan Brinson <jonathanbrinsontenor@gmail.com>
2020-12-02 19:38:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,9 +3,5 @@ def reverse_each_word(sentence)
   new_arr = sentence.split
   new_arr.collect do |element|
     rev_arr = sentence.reverse
-    binding.pry
-    rev_arr.each do |int_ele|
-      
-    end 
   end
 end 
